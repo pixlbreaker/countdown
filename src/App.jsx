5bb872107e8ab4438a7675a228c189e4db88b543
Michael Skotar <michaelskotar@gmail.com>
2025-06-30 16:45:14 -0400

    created the countdown app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React, { useState } from 'react';
+
+function App() {
+
+
+  return (
+    <div>
+      Countdown App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/words.js
@@ -0,0 +1,4 @@
+// src/words.js
+export const WORDS = [
+    'read', 'dear', 'dare', 'trade', 'rated', 'alert', 'alter', 'later', 'batter', 'battle', // etc.
+];
