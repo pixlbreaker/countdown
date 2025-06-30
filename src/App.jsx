dc8da4dc49ed60311e7c86e1ff8dac1fee2e5262
Michael Skotar <michaelskotar@gmail.com>
2025-06-30 17:31:03 -0400

    Generates a random number

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,20 @@ import React, { useState } from 'react';
 
 function App() {
 
+  const [target, setTarget] = useState(null);
+
+
+  const createTargetNumber = () => {
+    let randTarget = Math.floor(Math.random() * 900) + 100;
+    setTarget(randTarget);
+  }
 
   return (
     <div>
       Countdown App
+      <button onClick={createTargetNumber}>Generate Target</button>
+
+      {target && <h2>Target: {target}</h2>}
     </div>
   );
 }
