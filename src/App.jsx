31bca861a51aa32f5a3ce19cb95653af1f220194
Michael Skotar <michaelskotar@gmail.com>
2025-07-02 16:56:47 -0400

    Added an auto update for the check number

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import React, { useState } from 'react';
 function App() {
 
   const [target, setTarget] = useState(null);
-
+  const [guess, setGuess] = useState(null);
 
   const createTargetNumber = () => {
     let randTarget = Math.floor(Math.random() * 900) + 100;
     setTarget(randTarget);
+  }
+
+  const checkNumber = () => {
+    console.log(guess);
   }
 
   return (
@@ -17,6 +21,11 @@ function App() {
       <button onClick={createTargetNumber}>Generate Target</button>
 
       {target && <h2>Target: {target}</h2>}
+
+      <input value={guess} onChange={(e) => { setGuess(e.target.value) }} type='number'></input>
+      <button onClick={checkNumber}>Check Number</button>
+
+      {guess && <h3>Guess: {guess}</h3>}
     </div>
   );
 }
